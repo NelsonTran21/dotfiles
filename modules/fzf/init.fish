51883580083610aacef798809a60e67f5f079460
Nelson Tran <nelson@tran21.com>
2022-06-05 00:19:04 -0700

    Improve fuzzy finding behavior
    * Use `fd` instead of `rg` for the default command.
    
    * Update `cdf` abbreviation to use `fzf-cd-widget`. This works better
      with using Ctrl+c to abort the `cd` operation.
    
    * Hide the preview window by default. It can be enabled with `?`.
    
    * Improve preview output for files, directories, and other items.

--- modules/fzf/init.fish
+++ modules/fzf/init.fish
@@ -1,13 +1,24 @@
 if type -q fd # config.modules.fd.enable
-  abbr --add cdf "cd (fd --type directory | fzf)"
+  set -gx FZF_DEFAULT_COMMAND "fd --hidden --follow --exclude '.git' --exclude 'node_modules'"
+  set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+  set -gx FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND --type d"
+
+  abbr --add cdf "fzf-cd-widget"
 else
   abbr --erase cdf
 end
 
-if type -q rg # config.modules.rg.enable
-  set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git'"
-end
-
 if type -q bat # config.modules.bat.enable
-  set -gx FZF_DEFAULT_OPTS "--multi --preview 'bat --color=always {}'"
+  if type -q exa # config.modules.exa.enable
+    set PREVIEW_FILE "bat --color=always {} 2>/dev/null"
+    set PREVIEW_DIRECTORY "exa --tree {} 2>/dev/null"
+    set PREVIEW_NONE "echo Nothing to preview."
+
+    set -gx FZF_DEFAULT_OPTS "
+      --multi
+      --preview-window=:hidden
+      --preview '$PREVIEW_FILE; or $PREVIEW_DIRECTORY; or $PREVIEW_NONE'
+      --bind '?:toggle-preview'
+    "
+  end
 end
