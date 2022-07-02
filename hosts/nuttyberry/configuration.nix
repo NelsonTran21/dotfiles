18aa8f83f6c3b3dd48c3a3bb5e8ec9b049cc1d8b
Nelson Tran <nelson@tran21.com>
2022-07-01 20:15:02 -0700

    Add `nuttyberry` configuration to Nix flake
    - Generated wifi password with `wpa_passphrase <ssid> <password>`.
    - Generated user password with `mkpasswd -m sha-512`.

---
+++ hosts/nuttyberry/configuration.nix
@@ -0,0 +1,52 @@
+{ inputs, config, pkgs, ... }:
+
+let
+  wifiPassword = "e786ddb36c4864f3b95a5bbca4ab91e12b4555531fd69b7dbb10ab082d345e3b";
+  userPassword = "$6$SdzEEj7pMDAzRmT9$S.gFAPBbk3O7MsOfgBLlTmy3KraFDSCqMU21DQiQVxOxGICnIdJREMI/kO.w1TRnPxlj/LBdcHZszOu2TBn4C/";
+in
+{
+  imports = [
+    inputs.hardware.nixosModules.raspberry-pi-4
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = "nuttyberry";
+    wireless = {
+      enable = true;
+      networks.CentralPerk.pskRaw = wifiPassword;
+      interfaces = [ "wlan0" ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ vim ];
+
+  services.openssh.enable = true;
+
+  users = {
+    mutableUsers = false;
+    users.nelson = {
+      isNormalUser = true;
+      hashedPassword = userPassword;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  # Enable GPU acceleration
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+
+  services.xserver = {
+    enable = true;
+    displayManager.lightdm.enable = true;
+    desktopManager.xfce.enable = true;
+  };
+
+  hardware.pulseaudio.enable = true;
+}
