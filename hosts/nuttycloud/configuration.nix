11c0f9a211aa86c096d38bc1c307730aa65fc1cd
Nelson Tran <nelson@tran21.com>
2022-07-09 20:26:07 -0700

    Move kexec-specific NixOS option into another file

--- hosts/nuttycloud/configuration.nix
+++ hosts/nuttycloud/configuration.nix
@@ -11,10 +11,6 @@ with lib;
 
   system.stateVersion = "22.05";
 
-  kexec = mkIf (isRescueSystem == true) {
-    autoReboot = false;
-  };
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
