9188b2ff4f24ffcd6c7f8e2c37b02b064d0d3766
Nelson Tran <nelson@tran21.com>
2022-07-09 21:52:15 -0700

    Enable SSH service

---
+++ modules/ssh/config.linux.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+
+with lib;
+mkIf config.modules.ssh.enable {
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  programs.ssh.extraConfig = readFile ./config;
+
+  modules.home.user = {
+    openssh.authorizedKeys.keyFiles = [
+      ./authorized_keys
+    ];
+  };
+}
