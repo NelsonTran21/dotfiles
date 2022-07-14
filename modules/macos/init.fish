f81db431a537c06dc6fd0b60c6396d10142ee4d8
Nelson Tran <nelson@tran21.com>
2022-07-13 18:09:37 -0700

    Disable automatic period substitution
    Holy shit. I figured out why `<leader> <leader>` in VSCode was not
    working properly sometimes. It's because macOS was substituting the
    second `<leader>` (space) with a period.
    
    Source: https://stackoverflow.com/a/69365081

--- modules/macos/init.fish
+++ modules/macos/init.fish
@@ -4,6 +4,7 @@ defaults write com.apple.dock "magnification" -bool "true"
 defaults write com.apple.dock "tilesize" -int "48"
 defaults write com.apple.dock "largesize" -int "72"
 
+defaults write NSGlobalDomain "NSAutomaticPeriodSubstitutionEnabled" -int "0"
 defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
 defaults write NSGlobalDomain "InitialKeyRepeat" -int "10"
 defaults write NSGlobalDomain "KeyRepeat" -int "1"
