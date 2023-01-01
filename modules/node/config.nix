8e8a884ffabb7e0f130144d5a301c9bc5c864367
Nelson Tran <nelson@tran21.com>
2022-12-31 17:27:40 -0700

    Set Node environment variables in `init.fish`

--- modules/node/config.nix
+++ modules/node/config.nix
@@ -3,15 +3,9 @@
 with lib;
 mkIf config.modules.node.enable {
   environment.systemPackages = with pkgs; [
+    bun
     nodejs
     nodePackages.typescript
     yarn
   ];
-
-  environment.variables = {
-    NPM_CONFIG_USERCONFIG = "/home/${config.modules.home.user.name}/.config/npm/config";
-    NPM_CONFIG_CACHE = "/home/${config.modules.home.user.name}/.config/npm";
-    NPM_CONFIG_PREFIX = "/home/${config.modules.home.user.name}/npm";
-    NODE_REPL_HISTORY = "/home/${config.modules.home.user.name}/node/repl_history";
-  };
 }
--- modules/node/init.fish
+++ modules/node/init.fish
@@ -1,1 +1,7 @@
 fish_add_path (yarn global bin)
+
+# TODO: These paths are specific to macOS. Update to support NixOS.
+set -gx NPM_CONFIG_USERCONFIG /Users/nelson/.config/npm/config
+set -gx NPM_CONFIG_CACHE /Users/nelson/.config/npm
+set -gx NPM_CONFIG_PREFIX /Users/nelson/npm
+set -gx NODE_REPL_HISTORY /Users/nelson/node/repl_history
