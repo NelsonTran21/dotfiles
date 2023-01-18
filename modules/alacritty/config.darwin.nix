e996e600edacc1507d3b1972c87e4cae4766e9fb
Nelson Tran <nelson@tran21.com>
2023-01-18 10:53:27 -0700

    Add `alacritty.yml` config file

--- modules/alacritty/config.darwin.nix
+++ modules/alacritty/config.darwin.nix
@@ -5,4 +5,10 @@ mkIf config.modules.alacritty.enable {
   # Gatekeeper will not allow alacritty to run because it has not been notarized.
   # As a workaround, run `xattr -rd com.apple.quarantine /Applications/Alacritty.app`
   modules.homebrew.casks = [ "alacritty" ];
+
+  modules.home.file = {
+    ".config/alacritty/alacritty.yml" = {
+      source = ./alacritty.yml;
+    };
+  };
 }
