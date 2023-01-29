2936a779dd90e0d6c4029268572ac64c69600b76
Nelson Tran <nelson@tran21.com>
2023-01-29 00:42:40 -0700

    Pin VSCode installation to 1.73.1

--- modules/vscode/config.darwin.nix
+++ modules/vscode/config.darwin.nix
@@ -2,7 +2,13 @@
 
 with lib;
 mkIf config.modules.vscode.enable {
-  modules.homebrew.casks = [ "visual-studio-code" ];
+  # [HACK] Disabling VSCode installation via homebrew module because versions
+  # 1.74 breaks support for the Customize UI and Monkey Patch extensions that I
+  # use to customize the look and feel of VSCode. Pinning my installation to
+  # 1.73 for the unforeseeable future, or until the breaking changes are
+  # reverted by the VSCode team.
+
+  # modules.homebrew.casks = [ "visual-studio-code" ];
 
   # [NOTE] Prefer to manually symlink link VSCode settings to the dotfiles
   # repository instead of linking them directly to the Nix store. Extensions
---
+++ modules/vscode/install-vscode-1.73.fish
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/Homebrew/homebrew-cask/a9cffeefd3c9e0466a2e68e7525273000e8c498f/Casks/visual-studio-code.rb
+brew install -s visual-studio-code.rb
+rm visual-studio-code.rb
