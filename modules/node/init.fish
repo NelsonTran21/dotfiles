7c898dcf35afc171a970b102894c5155537ce36b
Nelson Tran <nelson@tran21.com>
2023-01-30 19:11:31 -0700

    Update NPM environment variables

--- modules/node/init.fish
+++ modules/node/init.fish
@@ -2,6 +2,6 @@ fish_add_path (yarn global bin)
 
 # TODO: These paths are specific to macOS. Update to support NixOS.
 set -gx NPM_CONFIG_USERCONFIG /Users/nelson/.config/npm/config
-set -gx NPM_CONFIG_CACHE /Users/nelson/.config/npm
-set -gx NPM_CONFIG_PREFIX /Users/nelson/npm
+set -gx NPM_CONFIG_CACHE /Users/nelson/.cache/npm
+set -gx NPM_CONFIG_PREFIX /Users/nelson/.config/npm
 set -gx NODE_REPL_HISTORY /Users/nelson/node/repl_history
