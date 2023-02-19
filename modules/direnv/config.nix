10af1b1275a5979f8d8dcd77c149557c8c51da87
Nelson Tran <nelson@tran21.com>
2023-02-19 16:33:19 -0700

    Add direnv configuration for loading `.env` files

--- modules/direnv/config.nix
+++ modules/direnv/config.nix
@@ -3,4 +3,10 @@
 with lib;
 mkIf config.modules.direnv.enable {
   environment.systemPackages = with pkgs; [ direnv ];
+
+  modules.home.file = {
+    ".config/direnv/direnv.toml" = {
+      source = ./direnv.toml;
+    };
+  };
 }
